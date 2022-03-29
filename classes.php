e7584f423cf28db55e93cbcb3471218ef0951f0c
jens5962 <jensrand1999@gmail.com>
2022-03-29 09:38:43 +0200

    Create classes.php

---
+++ classes.php
@@ -0,0 +1,40 @@
+<?php
+Class starkApp{
+
+}
+
+Class productGallery{
+
+}
+
+Class logIn{
+
+}
+
+Class customerService{
+
+}
+
+Class shoppingCart{
+
+}
+
+Class dateTimeProduct{
+
+}
+
+Class checkout{
+
+}
+
+
+$starkApp = new starkApp;
+$productGallery = new productGallery;
+$logIn = new logIn;
+$customerService = new customerService;
+$shoppingCart = new shoppingCart;
+$dateTimeProduct = new dateTimeProduct;
+$checkout = new checkout;
+
+
+?>
